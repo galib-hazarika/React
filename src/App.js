d9af2113abd828fc4fba73bd195dcff0c889665c
galib135 <hazarika.galib@gmail.com>
2023-11-24 13:39:28 +0530

    Clear button logic is being added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import { useState } from "react";
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-];
+// let initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: false },
+// ];
 
 function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
 }
-function Form() {
+function Form({ items, onAddItems }) {
   const [description, setInput] = useState("");
   const [quantity, setQuantity] = useState(1);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const newQuantity = {
-      id: Number(initialItems[initialItems.length - 1].id) + 1,
+      id: Date.now(),
       description,
       quantity,
       packed: false,
     };
+    onAddItems(newQuantity);
     console.log(newQuantity);
     setInput("");
     setQuantity(1);
@@ -45,41 +47,117 @@ function Form() {
     </form>
   );
 }
-function PackingList() {
+function PackingList({ items, onDeleteItem, onTogglePacked, onClearList }) {
+  //So select is now controlled element and its state should govern by useState hook
+  const [sort, setSort] = useState("input");
+
+  let sortedItems;
+
+  if (sort === "input") sortedItems = items;
+  if (sort === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sort === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
   return (
     <div className='list'>
       <ul>
-        {initialItems.map((item) => (
-          <Item item={item} />
+        {sortedItems.map((item) => (
+          <Item
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onTogglePacked={onTogglePacked}
+          />
         ))}
       </ul>
+      <div className='actions'>
+        <select value={sort} onChange={(e) => setSort(e.target.value)}>
+          <option value='input'>Sort by Input</option>
+          <option value='description'>Sort by Description</option>
+          <option value='packed'>Sort by Packed Status</option>
+        </select>
+        <button onClick={onClearList}>Clear List</button>
+      </div>
     </div>
   );
 }
-function Item({ item }) {
+function Item({ item, onDeleteItem, onTogglePacked }) {
   return (
     <li>
+      <input
+        type='checkbox'
+        value={item.packed}
+        onClick={() => onTogglePacked(item.id)}
+      />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
-function Stats() {
+function Stats({ items }) {
+  const currItemCount = items.length;
+  const packedItemCount = items.filter((item) => item.packed).length;
+  const packingPercent = Math.round((packedItemCount / currItemCount) * 100);
+  if (!currItemCount) {
+    return (
+      <footer className='stats'>
+        <em>Start adding some items to your packing list 🚀</em>
+      </footer>
+    );
+  }
   return (
     <footer className='stats'>
-      <em>You have X item in your list , you packed X (X%)</em>
+      <em>
+        {packingPercent === 100
+          ? "Wohooo 👉 your packing is done 😜😜"
+          : `You have ${currItemCount} item in your list , you packed
+          ${packedItemCount} item and packing percentage is${packingPercent}%`}
+      </em>
     </footer>
   );
 }
 function App() {
+  //Lifiting state to nearest parent component
+  const [items, setItem] = useState([]);
+  const handleAddItem = (item) => {
+    setItem((items) => [...items, item]);
+  };
+  const handleDeleteItem = (id) => {
+    setItem((items) => items.filter((item) => item.id != id));
+  };
+  const handleToggleItemPacked = (id) => {
+    setItem((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  };
+  const handleClearList = () => {
+    let message = window.confirm("Going to clear the list");
+    if (message) {
+      setItem([]);
+    } else {
+      setItem(items);
+    }
+  };
   return (
     <div className='app'>
       <Logo />
-      <Form />
-      <PackingList />
-      <Stats />
+      <Form items={items} onAddItems={handleAddItem} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onTogglePacked={handleToggleItemPacked}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
     </div>
   );
 }
